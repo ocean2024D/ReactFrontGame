622606b8af80fc13383b838db2b8f6c0da176d4f
Derya Lacin Maes- Full-Stack Developer <deryalacin@outlook.com>
2025-04-15 09:44:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   // useEffect dışında tanımlandı, böylece buton da kullanabilir
   const fetchData = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/game");
+      const response = await fetch("https://flaskgamebackend.onrender.com/api/game");
       const data = await response.json();
       console.log(data);
       setWord(data.randomWord);
